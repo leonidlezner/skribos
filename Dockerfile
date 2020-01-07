99ea8c51f040172eca8a220af89fc299f9996528
leonidlezner <kontakt@lezner.de>
2020-01-07 23:13:26 +0100

    Using git

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.10
 
+RUN apt-get update && apt-get install git -y
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@
 ls -la
 
 BUILD_DIR=$1
+GH_PAT=$2
 
 BRANCH_DIR="$BUILD_DIR/${GITHUB_REF:11}"
 
-echo "Output dir: $1 ${GITHUB_REF:11}"
+echo "Output dir: $1,  Branch dir: $BRANCH_DIR"
 
 mkdir -p $BRANCH_DIR
 
@@ -15,3 +16,5 @@ cp -r ./src "$BRANCH_DIR"
 list=`git tag --list 'v*'`
 
 echo $list > $BUILD_DIR/versions.txt
+
+echo $GH_PAT
