efe9c506d2561bae84666726a03f9f352372f6cd
leonidlezner <kontakt@lezner.de>
2020-01-07 22:41:28 +0100

    Try to list the folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh -l
 
-echo "Hello $1 and $GITHUB_REF"
+ls -la
+
+BUILD_DIR=$1
+
+BRANCH_DIR="$BUILD_DIR/${GITHUB_REF:11}"
+
+echo "Output dir: $1 ${GITHUB_REF:11}"
+
+mkdir -p $BRANCH_DIR
+
+cp ./src "$BRANCH_DIR"
