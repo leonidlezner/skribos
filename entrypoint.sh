c14a9f74d927a75041fd817fdae2ea6187029308
Leonid Lezner <leonidlezner@users.noreply.github.com>
2020-01-08 14:37:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ BRANCH_DIR="$BUILD_DIR/${GITHUB_REF:11}"
 
 mkdir -p $BRANCH_DIR
 
-
+pandoc src/metadata.yaml -s -o dist/lernOS-for-You-Guide-de.docx src/lernOS-for-You-Guide-de.md
 
 #git tag --list 'v*' > $BUILD_DIR/versions.txt
