57cca12b6659040bdc955335e70b095fb13d988f
Leonid Lezner <leonidlezner@users.noreply.github.com>
2020-01-08 14:29:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh -l
 
-ls -la
-
 BUILD_DIR=$1
 GH_PAT=$2
 BRANCH_DIR="$BUILD_DIR/${GITHUB_REF:11}"
 
-echo "Output dir: $1,  Branch dir: $BRANCH_DIR"
-
 mkdir -p $BRANCH_DIR
 
-cp -r ./src "$BRANCH_DIR"
 
-git tag --list 'v*' > $BUILD_DIR/versions.txt
+
+#git tag --list 'v*' > $BUILD_DIR/versions.txt
