fcdb16222df90f6e0fc170df4da1c41329682d2b
leonidlezner <kontakt@lezner.de>
2020-01-25 20:58:46 +0100

    Before merging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,13 @@
 #!/bin/sh -l
 
 CONTAINER_VERSION=$(cat /VERSION)
-BUILD_DIR=${1:-dist}
-RECIPE=${2:-skribos.yml}
-ENV=${3:-server}
+export BUILD_DIR=${1:-dist}
+export BUILD_ENV=${3:-server}
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-export PYTHONUNBUFFERED=1
-
 echo "Skribos Container $CONTAINER_VERSION"
-echo "Output: $BUILD_DIR, Recipe: $RECIPE, Environment: $ENV"
+echo "BUILD_DIR: $BUILD_DIR"
 
-mkdir -p $BUILD_DIR
-
-if [ $ENV = "local" ];
-then
-    SUFFIX='--nodownload'
-else
-    SUFFIX=''
-    echo "Updating Skribos CLI"
-    git -C /usr/local/skribos-cli/ fetch --tags
-    git -C /usr/local/skribos-cli/ pull origin master    
-fi
-
-# Run Skribos
-python3 /usr/local/skribos-cli/skribos.py --recipe $RECIPE --output $BUILD_DIR $SUFFIX
+exec "$@"
