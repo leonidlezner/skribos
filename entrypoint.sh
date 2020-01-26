8cf0861e61968ce98e29868e06b3bc5a4cf9958a
leonidlezner <kontakt@lezner.de>
2020-01-26 16:35:09 +0100

    Removing tools from the folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 CONTAINER_VERSION=$(cat /VERSION)
-export BUILD_DIR=${1:-dist}
+export BUILD_DIR=${2:-dist}
 export BUILD_ENV=${3:-server}
 
 export LC_ALL=C.UTF-8
@@ -9,5 +9,12 @@ export LANG=C.UTF-8
 
 echo "Skribos Container $CONTAINER_VERSION"
 echo "BUILD_DIR: $BUILD_DIR"
+
+if [ ! -d "./external/skribos-cli" ]; then
+  git clone https://github.com/leonidlezner/skribos-cli.git ./external/skribos-cli
+fi
+
+ln -s /data/external/skribos-cli/skribos_update.sh /usr/bin/skribos_update.sh
+ln -s /data/external/skribos-cli/skribos_build.sh /usr/bin/skribos_build.sh
 
 exec "$@"
